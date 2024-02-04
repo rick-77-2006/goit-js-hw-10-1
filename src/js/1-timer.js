637f125f2b0e632f7e2d4ade4226a395b24d4b34
Rick <46992687+rick-77-2006@users.noreply.github.com>
2024-02-04 09:21:31 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -76,7 +76,6 @@ function stopTimer() {
   } 
 };
 
-
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 };
@@ -86,12 +85,10 @@ function convertMs(ms) {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   const days = Math.floor(ms / day);
   const hours = Math.floor((ms % day) / hour);
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 };
 
